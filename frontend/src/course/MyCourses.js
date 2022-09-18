f8829ca57e6d72afd456a41b3721569df9759206
OmarYasser <omargyasser@gmail.com>
2022-09-18 11:55:20 +0200

    Feat: implemented MyCourses componet that shows courses per instructor

--- frontend/src/course/MyCourses.js
+++ frontend/src/course/MyCourses.js
@@ -1,6 +1,7 @@
 import {useState, useEffect} from "react";
 import {isAuthenticated} from "../auth/auth-helper"
 import { listCourses } from "./api-course";
+import {ListItemText, Typography, List, ListItem, ListItemAvatar, Avatar, Paper} from "@mui/material"
 const MyCourses = () => {
     const [courses, setCourses] = useState([]);
     const jwt = isAuthenticated();
@@ -20,6 +21,32 @@ const MyCourses = () => {
             abortController.abort();
           };
     }, [userId, jwt])
-    return <>Your Courses
-    {courses.map((course) => (<></>))}</>}
+    return <Paper elevation={3} sx={{ maxWidth: 600, mx: 'auto', padding: 3 }}>
+    <Typography variant='h5'> Your Courses</Typography>
+
+   
+<List sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}>
+      {courses.map((course)=>(
+        <ListItem alignItems="flex-start">
+        <ListItemAvatar>
+            <Avatar alt={`${course.name} image`} variant="square" src={`/api/courses/${course.image}/image`} />
+        </ListItemAvatar>
+        <ListItemText
+            primary={`${course.name}`}
+            secondary={
+            <>
+                <Typography
+                sx={{ display: 'inline' }}
+                component="span"
+                variant="body2"
+                color="text.primary"
+                >
+               {course.description}
+                </Typography>
+                
+            </>
+            }
+        />
+      </ListItem>))}
+</List></Paper>}
 export default MyCourses
