d98c820fc5fe7d20c65515b6a3f9bd929ce9b521
OmarYasser <rawda.developer@gmail.com>
2022-10-03 14:10:13 +0200

    Feat: added getEnrollments route and controller

--- backend/src/controllers/enrollment.controller.js
+++ backend/src/controllers/enrollment.controller.js
@@ -1,4 +1,5 @@
 import Enrollment from "../models/enrollment.model";
+import Course from "../models/course.model";
 export const enroll = async (req, res) => {
   const course = req.course;
   const user = req.profile;
@@ -27,5 +28,19 @@ export const enroll = async (req, res) => {
     return res.json(result);
   } catch (err) {
     return res.status(400).json({ error: "Couldn't enroll in course" });
+  }
+};
+export const getEnrollments = async (req, res) => {
+  const user = req.profile;
+  try {
+    const courses = await Enrollment.find({ userId: user._id });
+    const coursesRes = [];
+    for (let course of courses) {
+      const result = await Course.findById(course.courseId);
+      coursesRes.push(result);
+    }
+    return res.status(200).json(coursesRes);
+  } catch (err) {
+    return res.status(400).json({ error: "Couldn't get enrolled course" });
   }
 };
--- backend/src/routes/enrollment.routes.js
+++ backend/src/routes/enrollment.routes.js
@@ -2,11 +2,14 @@ import express from "express";
 import { hasAuthorization, requireLogin } from "../controllers/auth.controller";
 import { userByID } from "../controllers/user.controller";
 import { courseByID } from "../controllers/course.controller";
-import { enroll } from "../controllers/enrollment.controller";
+import { enroll, getEnrollments } from "../controllers/enrollment.controller";
 const router = express.Router();
 router
   .route("/api/users/:userId/courses/:courseId/enroll")
   .post(requireLogin, hasAuthorization, enroll);
+router
+  .route("/api/users/:userId/courses/enrollments")
+  .get(requireLogin, hasAuthorization, getEnrollments);
 router.param("userId", userByID);
 router.param("courseId", courseByID);
 
